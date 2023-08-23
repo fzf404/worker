61b689f48f4d3e6f0584fa3583cbedca11871a1c
fzf404 <me@fzf404.art>
2023-08-23 16:30:09 +0800

    feat: support github proxy

--- cdn/index.js
+++ cdn/index.js
@@ -2,16 +2,50 @@ import { Router } from 'itty-router'
 
 const router = Router()
 
+router.get('/favicon.ico', () => new Response('Not Found', { status: 404 }))
+
 router.get(
   '/',
-  () => new Response('The URL structure is /package@version/file.js'),
+  () => new Response('The URL structure is /package@version/file'),
 )
+
+router.get(
+  '/gh',
+  () => new Response('The URL structure is /gh/package@version/file'),
+)
+
+router.get(
+  '/npm',
+  () => new Response('The URL structure is /npm/package@version/file'),
+)
+
+router.get('/gh/*', async (request) => {
+  const url = new URL(request.url)
+  const path = url.pathname.replace('/gh/', '')
+  const response = await fetch(`https://cdn.jsdelivr.net/gh/fzf404/${path}`)
+  if (response.status === 400) {
+    return new Response('The URL structure is /gh/package@version/file')
+  }
+  return response
+})
+
+router.get('/npm/*', async (request) => {
+  const url = new URL(request.url)
+  const path = url.pathname.replace('/npm/', '')
+  const response = await fetch(`https://cdn.jsdelivr.net/npm/${path}`)
+  if (response.status === 400) {
+    return new Response('The URL structure is /npm/package@version/file')
+  }
+  return response
+})
 
 router.get('/*', async (request) => {
   const url = new URL(request.url)
-  const link = `https://www.unpkg.com${url.pathname}`
-  return await fetch(link)
+  const path = url.pathname.replace('/', '')
+  return await fetch(`https://www.unpkg.com/${path}`)
 })
+
+router.all('*', () => new Response('Not Found', { status: 404 }))
 
 export default {
   async fetch(request) {
